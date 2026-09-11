f49e6bb168f988b30fb0af9820e2627bdef22fc5
Adriana Sveca <svecaadriana05gmail.com>
2026-09-11 17:54:21 +0100

    Used react to create an array to be able to scroll through.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,37 @@
 import "../styles/hero.css";
+import { useState, useEffect } from "react";
 
 function Hero() {
+    const myArray = [
+        'Software Developer',
+        'CS Student @ Aston',
+        'Software Engineer',
+        'Web Developer'
+    ];
+
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+
+        const timer = setInterval(() => {
+            setIndex(index => (index + 1) % myArray.length);
+        }, 3000);
+
+        return () => {
+            clearInterval(timer);
+        };
+
+    }, []);
+
+
+
+
+
     return (
         <>
             <div className="hero">
                 <div className="heroInformation"> <h1 className="heroHeading">ADRIANA SVECA</h1>
-                    <h4 className="heroSubHeading">Software Developer</h4>
+                    <h4 className="heroSubHeading">{myArray[index]}</h4>
                     <p className="heroDescription">I build web applications, games and
                         interactive experiences.</p></div>
 
