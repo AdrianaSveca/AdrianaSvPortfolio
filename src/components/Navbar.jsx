16ca68017da7bfe83fad97b5409a073d50f2466d
Adriana Sveca <svecaadriana05gmail.com>
2026-09-11 15:31:36 +0100

    Basic footer components started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "../styles/navbar.css";
+
+
+function Navbar() {
+    return (
+        <nav>
+            <h3>&lt;Adriana.dev /&gt;</h3>
+
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Clients</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
